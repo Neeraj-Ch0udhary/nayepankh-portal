4c56dd6ec625e1e12e27e0d378cb3a577860c863
Neeraj-Ch0udhary <neeraj89303@gmail.com>
2026-06-17 17:26:36 +0530

    Point frontend to live backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://nayepankh-portal.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
